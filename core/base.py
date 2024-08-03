fix logging loss with bacth gradient descent

core/base.py
@@ -161,6 +161,7 @@ class LogisticRegressionBatchGD(BaseLR):
         
         start = time()
         for _ in range(self.num_iterations):
+            total_loss = 0
             for batch_X, batch_y in data_loader:
                 z = np.dot(batch_X, self.theta)
                 h = super()._BaseLR__sigmoid(z)
@@ -171,10 +172,11 @@ class LogisticRegressionBatchGD(BaseLR):
                 z = np.dot(batch_X, self.theta)
                 h = super()._BaseLR__sigmoid(z)
                 loss = super()._BaseLR__loss(h, batch_y, self.theta)
+                total_loss += loss
                 self.step_history.append(loss)
             
             if self.log == True:
-                self.logging_loss(X, y, self.theta)
+                self.history.append(total_loss / len(data_loader))
                 self.times.append(time() - start)
     
 
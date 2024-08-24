a9d1ff70074bfe32b585ff9ecbf5540ac1dd01b4
QuyAnh <dangquyanh150101@gmail.com>
2024-08-24 17:55:02 +0700

    reduce flops for backtrack gd

--- core/base.py
+++ core/base.py
@@ -118,8 +118,9 @@ class LogisticRegressionGD(BaseLR):
 
     def __find_optimal_learning_rate(self, learning_rate, rho, c, gradient, X, y):
         alpha = learning_rate
+        gradient_norm_sq = np.linalg.norm(gradient)**2
         while super()._BaseLR__loss(super()._BaseLR__sigmoid(np.dot(X, self.theta - alpha * gradient)), y) > \
-              super()._BaseLR__loss(super()._BaseLR__sigmoid(np.dot(X, self.theta)), y) - c * alpha * (np.linalg.norm(gradient)**2):
+              super()._BaseLR__loss(super()._BaseLR__sigmoid(np.dot(X, self.theta)), y) - c * alpha * gradient_norm_sq:
             alpha *= rho
 
         return alpha
